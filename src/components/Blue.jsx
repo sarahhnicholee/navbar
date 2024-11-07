c01cee4131c97fbb668f3f161d09558e03aa7f06
Sarah Parent <sarahparent@Sarahs-MacBook-Air.local>
2024-11-07 12:56:03 -0600

    initial

---
+++ src/components/Blue.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import App from './App.jsx'
+
+export default function Blue() {
+  return (
+    <div className= "blue">
+      <h1>Blue</h1>
+    </div>
+  )
+}
---
+++ src/components/Red.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import App from './App.jsx'
+
+export default function Red() {
+  return (
+    <div className= "red">
+      <h1>Red</h1>
+    </div>
+  )
+}
